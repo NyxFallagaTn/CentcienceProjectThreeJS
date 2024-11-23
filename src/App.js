56f3a57c0d27ed4bbb666b101e737e7bec7b5b00
NyxFallagaTn <29281498+NyxFallagaTn@users.noreply.github.com>
2024-11-23 02:54:21 +0100

    adding files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Floor from './Floor'; // Import the Floor component
+
+const App = () => {
+  return (
+    <div>
+      <Floor /> {/* Render the Floor component */}
+    </div>
+  );
+};
+
+export default App;
